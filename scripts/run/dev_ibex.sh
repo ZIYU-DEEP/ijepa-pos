a4f4888b93ed7fcb90b1f7d6c853a3978addd0f9
hazel <ziyuye@uchicago.edu>
2024-01-28 01:58:35 -0500

    update the bash

--- scripts/run/dev_ibex.sh
+++ scripts/run/dev_ibex.sh
@@ -1,3 +1,7 @@
 python main_pos.py \
   --fname configs/dev_ibex.yaml \
   --devices cuda:0 cuda:1 cuda:2 cuda:3
+
+python main_pos.py \
+  --fname configs/dev_ibex_pos.yaml \
+  --devices cuda:0 cuda:1 cuda:2 cuda:3
